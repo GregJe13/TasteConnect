8d86527c89fe1ef9e7e55cc864cdbaff91a9dce8
GregJe13 <126569840+GregJe13@users.noreply.github.com>
2025-06-15 00:20:28 +0700

    fix sweetalert2

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -10,6 +10,15 @@
             })
         </script>
     @endif
+    @if (session()->has('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: '{{ session('success') }}',
+            })
+        </script>
+    @endif
 
     <section class="bg-gray-100">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
